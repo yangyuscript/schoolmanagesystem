7bd667dc4d645543452401a6dc463b24299cb616
yangyuscript <1837475870@qq.com>
2019-01-06 22:10:19 +0800

    连接后台应用实现登录

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,116 +1,80 @@
-//登陆界面
-//已由网络后台修改为系统后台
+var request = require('../../utils/request.js')
 //获取应用实例
 var app = getApp()
-var userName = {}
-var userPassword = {}
-var flag = {}//用户身份的标志，是否是主任
-var that = {}
 Page({
   data: {
     motto: "教研室小助手",
     userName: '',
-    userPassword: '',
-    flag: '',
-    name:'',
-    picture:'',
-    usernum: '',
-    that: '',
+    password: ''
   },
-  /************************* */
-  studentID: function (e) {
-    that.data.userName = e.detail.value
+  studentID: function(e) {
+    this.data.userName = e.detail.value
   },
-  /************************* */
-  classID: function (e) {
-    that.data.userPassword = e.detail.value
+  passwordID: function(e) {
+    this.data.password = e.detail.value
   },
-  /************************* */
-  onLoad: function (options) {
-    that = this;
+  onLoad: function(options) {
+
   },
-  /************************* */
-  login: function () {
+  login: function() {
     console.log("what the fuck")
-    wx.switchTab({
-      url: '../personal/personal'
-    });
-    /*app.getuserid = this.userName
-    var headerObj = {
-      'Content-Type': 'application/json'
-    };
-    wx.request({
-      url: app.totalURL + '/test/login',//发送教师账号（usernum）、密码（password）接口，返回用户账号密码是否合法result，以及用户的（flag）数据（Number）类型
-      method: "GET",
-      data: {
-        userId: that.data.userName,
-        userPs: that.data.userPassword
+    var $that = this
+    request.GET('/index/login', {
+      params: {
+        userName: $that.data.userName,
+        password: $that.data.password
       },
-      header: headerObj,
-      success: function (res) {
-        console.log("发送成功！输入的账号为：" + that.data.userName + "密码为：" + that.data.userPassword + "结果为：" + res.data)
-        if (res.data.result == 'false') {
+      success: function(res) {
+        console.log("登录成功：")
+        console.log(res.data.result)
+        if (res.data.result != null) {
+          if (res.data.result != 'no') {
+            wx.setStorage({
+              key: 'userName',
+              data: $that.data.userName,
+            })
+            wx.setStorage({
+              key: 'password',
+              data: $that.data.password,
+            })
+            wx.switchTab({
+              url: '../index/index',
+            })
+          } else {
+            console.log('登录失败！')
+            wx.showToast({
+              title: '输入的学号密码不正确，请重试',
+              icon: 'none',
+              duration: 2000
+            })
+          }
+        } else {
+          console.log('登录失败！')
           wx.showToast({
-            title: '账户密码错误！',
-            icon: 'success',
+            title: '输入的学号密码不正确，请重试',
+            icon: 'none',
             duration: 2000
-          }),
-          wx.navigateTo({
-            url: '../login',
-          })
-        }else{
-        that.setData({
-          flag: res.data.flag,
-          name:res.data.name,
-          picture:res.data.picture,
-        });
-        app.getuserid = that.data.usernum;
-        app.userId=that.data.usernum;
-        app.flag = that.data.flag;
-        app.name = that.data.name;
-        app.picture = that.data.picture;
-        if (userPassword == "123456") {
-          wx.showModal({
-            title: '提示',
-            content: '密码太简单，跑去改一下吧~',
-            success: function (res) {
-              if (res.confirm) {
-                wx.navigateTo({
-                  url: '../changePwd/changePwd'
-                });
-              } else if (res.cancel) {
-                wx.redirectTo({
-                  url: '/pages/personal/personal',
-                })
-                // if (flag == 1) {//“1”表示是普通老师；“0”表示是主任
-                //   wx.navigateTo({
-                //     url: '../keshe1/keshe1'
-                //   });
-                // } else {
-                //   wx.navigateTo({
-                //     url: '../keshe/keshe'
-                //   });
-                // }
-              }
-            }
           })
         }
-      }
       },
-      false: function (error) {
-        console.log(error)
+      fail: function() {
+        console.log('登录失败！')
+        wx.showToast({
+          title: '请检查您的网络',
+          icon: 'loading',
+          duration: 2000
+        })
       }
-    })*/
+    })
   },
-  forget: function () {
+  forget: function() {
     wx.navigateTo({
       url: '../findpsw/findpsw'
     });
   },
-  regist: function(){
+  regist: function() {
     wx.navigateTo({
       url: '../regist/regist',
     })
   }
 })
-
---
+++ utils/request.js
@@ -0,0 +1,51 @@
+var API_URL = 'http://lingx.free.idcfengye.com'
+//var API_URL = 'https://sharebooks.yangyuscript.club/api/'
+
+var requestHandler = {
+  params: {},
+  success: function (res) {
+    //success
+  },
+  fail: function () {
+
+  }
+}
+
+//GET请求
+function GET(controlPath, requestHandler) {
+  request('GET', controlPath, requestHandler)
+}
+
+//POST请求
+function POST(controlPath, requestHandler) {
+  request('POST', controlPath, requestHandler)
+}
+
+function request(method, controlPath, requestHandler) {
+  //注意，可以对params加密等处理
+  var params = requestHandler.params;
+
+  wx.request({
+    url: API_URL + controlPath,
+    data: params,
+    method: method,//OPTIONS,GET,HEAD,POST,PUT,DELETE,TRACE,CONNECT
+    header: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "x-access-token": wx.getStorageSync('token')
+    },//设置请求头
+    success: function (res) {
+      requestHandler.success(res)
+    },
+    fail: function () {
+      requestHandler.fail()
+    },
+    complete: function () {
+      //complete
+    }
+  })
+}
+
+module.exports = {
+  GET: GET,
+  POST: POST
+}
